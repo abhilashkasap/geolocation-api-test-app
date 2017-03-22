942d9d7b7c490de270e386e7bd169be65b32a190
VIKSHIPT <VIKSHIPT>
2017-03-22 11:32:47 +0530

    done

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var http = require('http').createServer(app);
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/checkapi', function(req, res) {
+
+    req.send("You are too dumb !!! ");
+})
 var port = process.env.PORT || 8082;
 http.listen(port, function() {
     console.log('The sever listening at ${port} !');
